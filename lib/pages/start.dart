077d4a06305d3c5510826dbc804589ed6c4688ac
NicholasDeFelice <61028830+NicholasDeFelice@users.noreply.github.com>
2020-03-02 13:35:38 -0500

    Add files via upload

--- lib/pages/start.dart
+++ lib/pages/start.dart
@@ -17,48 +17,50 @@ class _LoadPageState extends State<LoadPage>{
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Welcome!", textAlign: TextAlign.center,),
-      ),
       body: Container(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-          AppCard(
-            child: Center(
-              child: Text("Stepping Stone", style: TextStyle(fontSize: 32.0, color: selection), textAlign: TextAlign.center,),
-            ),
-          ),
-          AppCard(
-            child: Container(
-              margin: EdgeInsets.only(top: 20.0),
-              child: Column(
-                children: <Widget>[
-                  Text("Already have an account?", style: TextStyle(fontSize: 18.0, color: selection), textAlign: TextAlign.center,),
-                  Container(
-                    width: 200,
-                    margin: EdgeInsets.only(top: 20.0, bottom: 20.0),
-                    child: RaisedButton(
-                      color: selection,
-                      onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()),);},
-                      child: Text("Login", style: TextStyle(color: w),),
-                    )
-                  ),
-                  Text("Make a new account?", style: TextStyle(fontSize: 18.0, color: selection), textAlign: TextAlign.center,),
-                  Container(
-                    width: 200,
-                    margin: EdgeInsets.only(top: 20.0),
-                    child: RaisedButton(
-                      color: selection,
-                      onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => SigninPage()),);},
-                      child: Text("Sign Up", style: TextStyle(color: w),),
-                    )
-                  ),
-                ],
+            AppCard(
+              child: Center(
+                child: Column(
+                  children: <Widget>[
+                    Text("Welcome To", style: TextStyle(fontSize: 32.0, color: selection), textAlign: TextAlign.center,),
+                    Text("Stepping Stone!", style: TextStyle(fontSize: 32.0, color: selection), textAlign: TextAlign.center,),
+                  ]
+                ),
               ),
-            )
             ),
-        ],
+            AppCard(
+              child: Container(
+                margin: EdgeInsets.only(top: 20.0),
+                child: Column(
+                  children: <Widget>[
+                    Text("Already have an account?", style: TextStyle(fontSize: 18.0, color: selection), textAlign: TextAlign.center,),
+                    Container(
+                      width: 200,
+                      margin: EdgeInsets.only(top: 20.0, bottom: 20.0),
+                      child: RaisedButton(
+                        color: selection,
+                        onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()),);},
+                        child: Text("Login", style: TextStyle(color: w),),
+                      )
+                    ),
+                    Text("Make a new account?", style: TextStyle(fontSize: 18.0, color: selection), textAlign: TextAlign.center,),
+                    Container(
+                      width: 200,
+                      margin: EdgeInsets.only(top: 20.0),
+                      child: RaisedButton(
+                        color: selection,
+                        onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => SigninPage()),);},
+                        child: Text("Sign Up", style: TextStyle(color: w),),
+                      )
+                    ),
+                  ],
+                ),
+              )
+            ),
+          ],
         ),
       )
     );

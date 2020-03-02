501dad30cee14c46442680adc94466381d464620
NicholasDeFelice <61028830+NicholasDeFelice@users.noreply.github.com>
2020-03-02 11:30:55 -0500

    Add files via upload

---
+++ lib/common/globals.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+bool showPass = true;
+Color selection = Colors.blue[900];
+Color w = Colors.white;
+
+String pass1 = '';
+String newuser = '';
+String newemail = '';
+String pass2 = '';
+
+String pass = '';
+String user = '';
+String email = '';

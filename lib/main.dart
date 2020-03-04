e0baf45547f1c818a709aaff0c445dd108241731
NicholasDeFelice <61028830+NicholasDeFelice@users.noreply.github.com>
2020-03-04 10:09:07 -0500

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Stepping Stone',
       theme: ThemeData(primaryColor: selection),
-      home: LoadPage()
+      home: StartPage()
       );
   }
 }

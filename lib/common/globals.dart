e8017c8c99be534dd56d25f489317fe9a0534f99
NicholasDeFelice <61028830+NicholasDeFelice@users.noreply.github.com>
2020-05-12 15:26:35 -0400

    Add files via upload

--- lib/common/globals.dart
+++ lib/common/globals.dart
@@ -26,3 +26,32 @@ String p = "123456789";
 String userview;
 
 List<String> follow = [];
+
+
+String url;
+String url1;
+String url2;
+String sham = "https://firebasestorage.googleapis.com/v0/b/stepping-stone-flutter.appspot.com/o/shamwow.mp4?alt=media&token=17a680b7-6431-4ba5-9db8-0fb8955379e7";
+String squ = "https://firebasestorage.googleapis.com/v0/b/stepping-stone-flutter.appspot.com/o/media.io_DSCN1377.mp4?alt=media&token=4d6a5608-7718-43c8-a33e-96fad0a6008e";
+String url3;
+
+String pN1;
+String pT1;
+String pN2;
+String pT2;
+String pN3;
+String pT3;
+String abeNPOne = "ShamWow 2.0";
+String abeNPTwo = "Squerrel remover";
+String abeTPOne = "Revelutionary new cleaning product! It can now absorb twice as much liquid!";
+String abeTPTwo = "Birdfeader that stops squerrels from getting to it.";
+String gNOne = "First Project";
+String gNTwo = "Second Project";
+String gNThree = "Third Project";
+String gText = "No current project.";
+
+int aberec = 0;
+int bjornrec = 0;
+int rec = 0;
+
+String img;
